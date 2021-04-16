fe2a63215e36bef20dceca61d71277e02840265d
Jinyan Yu <yujinyan@outlook.com>
2021-04-16 23:34:42 +0800

    Add clikt, build uber jar (#7)
    * add clikt, build uber jar
    
    * graalvm no fallback
    
    * add test binary step in gh action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+  application
   kotlin("jvm") version "1.4.32"
   id("com.palantir.graal") version "0.7.2"
+  id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "me.yujinyan"
@@ -19,10 +22,21 @@ tasks.withType<KotlinCompile>().configureEach {
   }
 }
 
+tasks.withType<ShadowJar>().configureEach {
+  archiveFileName.set("klisp.jar")
+}
+
+application {
+  // https://github.com/johnrengelman/shadow/pull/612
+  // mainClass.set("MainKt")
+  mainClassName = "MainKt"
+}
+
 graal {
   javaVersion("11")
   mainClass("MainKt")
   outputName("klisp")
+  option("--no-fallback")
 }
 
 repositories {
@@ -32,5 +46,6 @@ repositories {
 dependencies {
   testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
   testImplementation("io.kotest:kotest-assertions-core:4.4.3")
+  implementation("com.github.ajalt.clikt:clikt:3.1.0")
   implementation(kotlin("stdlib"))
 }
